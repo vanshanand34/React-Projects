b263451e9e177a541991f7d494310698b2067b5e
Vansh Anand <anandvansh34@gmail.com>
2026-01-25 14:39:03 +0530

    fix: move credentials from the config file to .env

--- bookstore/backend/config.js
+++ bookstore/backend/config.js
@@ -1,3 +1,8 @@
-export const mongoDBUrl = "mongodb+srv://anandvansh34:MDB.p%403104@vanshproject.z0naq.mongodb.net/books-collection?retryWrites=true&w=majority&appName=VanshProject";
+
+import { configDotenv } from "dotenv";
+
+configDotenv();
+
+export const mongoDBUrl = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@vanshproject.z0naq.mongodb.net/books-collection?retryWrites=true&w=majority&appName=VanshProject`;
 
 export const PORT = 8080;
